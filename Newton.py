0a2c2690226896286b5bdd37783ecef8d88de0ce
Andrei Petruk <Andrei Petruk>
2017-09-19 21:56:08 +0100

    String reverse

---
+++ Newton.py
@@ -0,0 +1,14 @@
+import math
+
+x = float(input("Enter a number: "))
+y = float(input("Sqrt guess: "))
+
+print("Math.sqrt(x): \t" + str(math.sqrt(x))) # Accurate sqrt
+
+approx = y - ((y*y - x) / (2 * y))
+
+for i in range (0,3):
+    z = approx
+    approx = y - ((y*y - x) / (2 * y))
+
+print("Approximate: \t" + str(approx))
---
+++ ReverseString.py
@@ -0,0 +1,3 @@
+word = input("Enter a word: ")
+print("Original: " + str(word))
+print("Reverse: " + str(word)[::-1])
